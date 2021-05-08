0c988a32bd6fb9b2777fe89406b775c1cd973188
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-05-08 19:24:23 -0400

    updates for netlify deploy

--- film/src/services/api-config.js
+++ film/src/services/api-config.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 //originally keep baseURL=localhost:3000
 //make sure to grab the heroku.com link not heroku.api 
-
+const baseUrl=process.env.NODE_ENV==='production' ? 'https://films-netflix.herokuapp.com': 'http://localhost:3000'
 const api = axios.create({
-  baseURL: `http://localhost:3000`
+  baseURL: baseUrl
 })
 
 export default api;
